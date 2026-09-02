2b90ad7242165b37274c26de123326df2176fc3a
aleksandra.mitjusova@tptlive.ee <aleksandra.mitjusova@tptlive.ee>
2026-09-02 13:21:19 +0300

    add variable changes

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-echo "Hello, World!";
+echo "Hello, World!"; //Writes out "Hello, World!" to the browser
+// hello
+// world
+
+
+/**
+ * 
+ * long comment
+ * some cool stuff
+ */
+
+$name = "John Doe"; // This is a variable that stores a name
+$age = 30; // This is a variable that stores an age
+echo age;
+$age = $age * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10; // update the age  variable by multiplying it by a series of numbers
+echo $age; // output the update age 
+echo 2+2; // output the result of 2 + 2
 ?>
