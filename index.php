663c72152a74e5d2ff37dea1f47a0a4c673303d5
Aleksandra Mitjusova <aleksandra.mitjusova@techno.ee>
2026-09-08 11:14:01 +0300

    add if and swtch case

--- index.php
+++ index.php
@@ -1,18 +1,35 @@
 <?php
-$test = true;
-$test = false;
-$test = true && true;
-$test = true || false;
-$test = !false;
-$test = 10 > 5;
-$test = 10 < 5;
-$test = 10 == 10;
-$test = 10 != 5;
-$test = 10 >= 10;
-$test = 10 <= 5;
-$test = 10 === 10;
-$test = 10 !== 5;
-$test = 10 === '10';
-$test = 10 !== '10';
-$test = true && false || (true && !true);
-var_dump($test);
+$num = 10;
+if($num > 5) { 
+    var_dump('bigger');
+}
+
+if ($num > 5){
+    var_dump('bigger');
+    } else if ($num < 5) {
+ var_dump ('smaller');
+} else {
+    var_dump('equal');
+}
+
+$i = 1;
+
+switch($i){
+    case 1:
+        var_dump('yx')
+        break;
+        case 2:
+var_dump('kaks')
+break;
+            case 3:
+               case 4:
+                var_dump('kolm neli')
+                break;
+                case 5:
+                    var_dump('viis')
+                    case 6:
+                        var_dump('kuus')
+                        break;
+                        default:
+                        var_dump('dunno');
+}
