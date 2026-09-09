5e1e9a17fc6680f511af207b8707c294fbd4a9a6
aleksandra mitjusova <aleksandra.mitjusova@techno.ee>
2026-09-09 13:44:03 +0300

    add inheritance

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
 class box {
-    public $width;
-    public $height;
-    public $length;
+    public int $width;
+    private int $height;
+    protected int $length;
     public $isOpen = false;
     public $hasBeenOpened = false;
 
@@ -19,14 +19,35 @@ class box {
     public function volume () {
         return $this->height * $this->width * $this->length;
     }
+
+    public function test() {
+        var_dump($this->height);
+    }
+
+    public function setHeight($height) {
+        if ($height < 0) {
+            $this->height = $height;
+        }
+    }
+
+    public function getHeight() {
+        return $this->height;
+    }
 }
 
 
-$box1 = new box();
-$box1->width = 10;
-$box1->height = 20;
-$box1->length = 30;
-$box1->open();
-var_dump($box1);
-var_dump($box1->volume());
-var_dump($box1);
+class Metalbox extends box {
+    public $weight;
+
+    public function mass() {
+        return $this->volume() * $this->weight;
+    }
+    public function test() {
+        var_dump($this->height);
+    }
+}
+
+$metal1 = new Metalbox();
+$metal1->height = 1;
+var_dump($metal1);
+ 
