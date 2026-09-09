67865a42bc9c40fd4938226d351fcbe6bdabd046
aleksandra mitjusova <aleksandra.mitjusova@techno.ee>
2026-09-09 10:03:18 +0300

    add class  object

--- index.php
+++ index.php
@@ -1,18 +1,32 @@
 <?php
-$test = true;
-$test = false;
-$test = true && true;
-$test = true || false;
-$test = !false;
-$test = 10 > 5;
-$test = 10 < 5;
-$test = 10 == 10;
-$test = 10 != 5;
-$test = 10 >= 10;
-$test = 10 <= 5;
-$test = 10 === 10;
-$test = 10 !== 5;
-$test = 10 === '10';
-$test = 10 !== '10';
-$test = true && false || (true && !true);
-var_dump($test);
+
+class box {
+    public $width;
+    public $height;
+    public $length;
+    public $isOpen = false;
+    public $hasBeenOpened = false;
+
+    public function open() {
+        $this->isOpen = true;
+        $this->hasBeenOpened = true;
+    }
+
+    public function close() {
+        $this->isOpen = false;
+    }
+
+    public function volume () {
+        return $this->height * $this->width * $this->length;
+    }
+}
+
+
+$box1 = new box();
+$box1->width = 10;
+$box1->height = 20;
+$box1->length = 30;
+$box1->open();
+var_dump($box1);
+var_dump($box1->volume());
+var_dump($box1);
